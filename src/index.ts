b2b8a742b36cac97192bb4bb7498bb76ca369c65
Beerer <bbeer.kritsada@gmail.com>
2025-03-17 20:45:39 +0700

    commit deploy

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,13 @@ fastify.addHook('onRequest', (request, reply, done) => {
     reply.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // กำหนดวิธีที่อนุญาต
     reply.header('Access-Control-Allow-Headers', 'Content-Type'); // กำหนด header ที่อนุญาต
     done();
-  });
+});
 
 
 fastify.get("/users", async (request, reply) => {
     return getUsers();
 });
-
-fastify.listen({ port: 3001 }, (err, address) => {
+fastify.listen({ port: 3000 }, (err, address) => {
     if (err) {
         fastify.log.error(err);
         process.exit(1);
